8f503f2afb1b66e8def12021f7c5b73bc0309db8
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-01-04 12:08:22 -0300

    update: Photos of Maratona SBC 2023

--- src/components/Photos/index.jsx
+++ src/components/Photos/index.jsx
@@ -7,11 +7,13 @@ import porto1 from '../../assets/images/porto1.png'
 import porto2 from '../../assets/images/porto2.png'
 import dojo1 from '../../assets/images/dojo1.png'
 import dojo2 from '../../assets/images/dojo2.png'
+import maratonaSBC1 from '../../assets/images/maratonaSBC1.png'
+import maratonaSBC2 from '../../assets/images/maratonaSBC2.jpg'
 
 export const Photos = () => {
     return (
         <div className={`col-10 heightFitContent bg-white p-2 rounded shadow-sm ${style.photosContainer}`}>
-            <p className='fw-semibold mb-1'>fotos <span className="textBlue">(4)</span></p>
+            <p className='fw-semibold mb-1'>fotos <span className="textBlue">(6)</span></p>
             <div className='d-flex justify-content-center'>
                 <MDBCarousel className={`w-75 ${style.carousel}`} showIndicators showControls fade>
                     <MDBCarouselItem
@@ -52,6 +54,26 @@ export const Photos = () => {
                     >
                         <h5>Coding Dojo da Liferay</h5>
                         <p>Evento para resolução de desafios de programação de modo conjunto pela comunidade tech de Recife, em fevereiro de 2022.</p>
+                    </MDBCarouselItem>
+
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={4}
+                        src={maratonaSBC1}
+                        alt='Maratona SBC 1'
+                    >
+                        <h5>Maratona SBC 2023 - Fase 1</h5>
+                        <p>Etapa regional da competição de programação organizada pela Sociedade Brasileira de Computação, em setembro de 2023.</p>
+                    </MDBCarouselItem>
+
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={4}
+                        src={maratonaSBC2}
+                        alt='Maratona SBC 2'
+                    >
+                        <h5>Maratona SBC 2023 - Fase 1</h5>
+                        <p>Etapa regional da competição de programação organizada pela Sociedade Brasileira de Computação, em setembro de 2023.</p>
                     </MDBCarouselItem>
                 </MDBCarousel>
             </div>
