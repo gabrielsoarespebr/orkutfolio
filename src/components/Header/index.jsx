e5ffacdeb321aef034f2d3835c6356b871ac41c4
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-03-27 08:48:01 -0300

    update: Resume URL

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
             <figure className={`h-100 m-0 d-flex justify-content-center align-items-center ${style.logoBg}`}>
                 <img className='h-75' src={orkutfolioLogo} alt="Orkutfolio Logo" />
             </figure>
-            <a className={style.resumeLink} href="https://www.canva.com/design/DAFZSPNeEJU/FIA75alCKNoKiPFylWl97Q/edit?utm_content=DAFZSPNeEJU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target='_blank'>Currículo</a>
+            <a className={style.resumeLink} href="https://www.canva.com/design/DAFxj7Qqcac/HNvBXYD63XDFy31F1hgWCw/edit?utm_content=DAFxj7Qqcac&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target='_blank'>Currículo</a>
         </header>
     )
 }
